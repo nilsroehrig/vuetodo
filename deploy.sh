3a7e8c4c835f5c82f3818132a45a6b8d8f4f803c
Nils Roehrig <der.user.00@gmail.com>
2018-11-30 10:20:31 +0100

    fix path issues; add new deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m "deploy #${CIRCLE_BUILD_NUM}"
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:drunknzombiecow/vuetodo.git master:gh-pages
+
+cd -
